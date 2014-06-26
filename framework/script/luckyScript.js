fb9d284e8da831fb42fb3abec59c16e4cc5eb6a2
morsky <vladvv2m@gmail.com>
2014-06-26 22:23:54 +0300

    Added lucky button with some wishes :)

---
+++ framework/script/luckyScript.js
@@ -0,0 +1,13 @@
+﻿function getWish() {
+  	
+  	// write wishes here
+	var wishes = ['Днес определено ти върви - за шестица на изпита се подготви.',
+    		 		'Късметлия днес си ти - домашното ще ти спори.',
+    				'Задачата трудна ще решиш - време е да се успокоиш.',
+    				'Днес в леглото остани - със задачите не ти върви.',
+    				'Тази вечер няма да се спи - задачи ще решаваш до зори.',
+    				''];
+    var positionInArray = Math.floor(Math.random() * wishes.length);
+
+    document.getElementById('text').innerHTML = wishes[positionInArray];
+}
--- framework/views/home.php
+++ framework/views/home.php
@@ -1,5 +1,13 @@
 <section>
 	<h1>Home page</h1>
 	<p>This is  homepage</p>
+	
+	<section>
+		<p>Изтегли си късметче: </p>
+		<button onclick="getWish()">Бутон на късмета</button>
+		<textarea id="text" placeholder="Днес ще..."></textarea>
+			
+		<script src="../script/luckyScript.js" ></script>
+	</section>
 </section>
 
