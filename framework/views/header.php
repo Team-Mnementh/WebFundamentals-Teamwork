c668121fa05a981aaebe0b8a96ca5f6e5873bc9a
morsky <vladvv2m@gmail.com>
2014-06-27 23:10:50 +0300

    Added header with navigation and simple styling.

--- framework/views/header.php
+++ framework/views/header.php
@@ -3,6 +3,19 @@
 	<head>
 		<meta charset="UTF-8">
 		<title><?= $data['title']?></title>
-		<link type="text/css" rel="stylesheet" href="css/styles.css">
+		<link type="text/css" rel="stylesheet" href="css/styles_v1.css">
 	</head>
 	<body>
+		<header>
+			<h1>Soft Uni <span>Fun Zone</span></h1>
+
+			<nav>
+				<ul>
+					<li><a href="#">Jokes</a></li>				
+					<li><a href="quiz.php">Quiz</a></li>
+					<li><a href="dreamer.php">Dreamer</a></li>
+					<li><a href="#">Pictures</a></li>
+					<li><a href="test.php">Video</a></li>
+				</ul>
+			</nav>
+	</header>
