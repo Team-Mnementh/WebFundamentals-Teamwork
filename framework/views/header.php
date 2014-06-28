83b8abb15d214a12e5645ea52762d64f75295034
morsky <vladvv2m@gmail.com>
2014-06-28 22:01:03 +0300

    Main page update. Added jokes page with three sections

--- framework/views/header.php
+++ framework/views/header.php
@@ -7,16 +7,22 @@
 	</head>
 	<body>
 		<header>
-			<h1>Soft Uni <span>Fun Zone</span></h1>
+		<h1>Soft Uni <span>Fun Zone</span></h1>
 
-			<nav>
-				<ul>
-					<li><a href="http://su.gloffice.eu">Main</a></li>
-					<li><a href="#">Jokes</a></li>			
-					<li><a href="http://su.gloffice.eu/quiz">Quiz</a></li>
-					<li><a href="http://su.gloffice.eu/dreamer">Dreamer</a></li>
-					<li><a href="#">Pictures</a></li>
-					<li><a href="http://su.gloffice.eu/test">Video</a></li>
-				</ul>
-			</nav>
-		</header>
+		<nav>
+			<ul>
+				<li><a href="http://su.gloffice.eu">Main</a></li>
+				<li><a href="#">Jokes</a>
+					<ul>
+						<li><a href="http://su.gloffice.eu/jokes">Jokes</a></li>
+						<li><a href="http://su.gloffice.eu/poems">Poems</a></li>
+						<li><a href="http://su.gloffice.eu/tales">Tales</a></li>
+					</ul>
+				</li>				
+				<li><a href="quiz.php">Quiz</a></li>
+				<li><a href="http://su.gloffice.eu/dreamer">Dreamer</a></li>
+				<li><a href="#">Pictures</a></li>
+				<li label="Видео"><a href="http://su.gloffice.eu/test">Video</a></li>
+			</ul>
+		</nav>
+	</header>
