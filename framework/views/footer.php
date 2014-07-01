7e9fbb7a5d4af25d34f5b60f02fdfb55d6694990
N-Hristov <nicolahristov93@gmail.com>
2014-07-01 21:02:37 +0300

    Fixed typos in footer.php #2

--- framework/views/footer.php
+++ framework/views/footer.php
@@ -8,13 +8,13 @@
 					<p>Валидатори за HTML5 и CSS3:</p>
 					
 					<a href="http://validator.w3.org">
-						<img src="../images/footer/html5.png" alt="html5.png" width="82" heigt="17" />
+						<img src="../images/footer/html5.png" alt="html5.png" width="82" height="17" />
 					</a>
-					<a href="http://css-validator.org"><img src="../images/footer/css3.png" alt="css3.png" width="82" heigt="17" /></a>
+					<a href="http://css-validator.org"><img src="../images/footer/css3.png" alt="css3.png" width="82" height="17" /></a>
 				</div>
 
 				<div>
-					<p>&#169;2014 Web site created by "Mnementh" &#153;</p>
+					<p>&#169;2014 Web site created by "Mnementh" &trade;</p>
 				</div>
 			</section>
 		</footer>
