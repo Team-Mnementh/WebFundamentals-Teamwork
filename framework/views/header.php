984e346b49f62c10c1d9d007e8389f2e3d74267a
N-Hristov <nicolahristov93@gmail.com>
2014-07-01 21:31:57 +0300

    Adding fancybox transitions

--- framework/views/header.php
+++ framework/views/header.php
@@ -18,6 +18,22 @@
         	$(document).ready(function() {
         		$(".fancybox").fancybox();
         	});
+			
+			$(".fancybox").fancybox({
+                'transitionIn' : 'elastic',
+                'transitionOut' : 'elastic',
+                'speedIn'  : 600,
+                'speedOut'  : 200,
+                'overlayShow' : false
+            });
+
+            $("а.fancybox").fancybox({
+                'transitionIn' : 'elastic',
+                'transitionOut' : 'elastic',
+                'speedIn'  : 600,
+                'speedOut'  : 200,
+                'overlayShow' : false
+            });
         </script>
 	</head>
 	<body>
