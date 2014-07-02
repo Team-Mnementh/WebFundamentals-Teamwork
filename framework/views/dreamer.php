f3d42f05dd6af96f046fe3c8c135f8d0b12694c1
orlinov <orlinov@users.noreply.github.com>
2014-07-02 20:42:33 +0300

    Dreamer design

--- framework/views/dreamer.php
+++ framework/views/dreamer.php
@@ -1,34 +1,34 @@
 <section class="dreamer">
-		<header>
-			 <h2>SoftUni съновник</h2>
-		</header> 
-
-<pre>
-switch ($dream) {
-	case1: "Наков ти говори на сън":
-		$result = "Добре дошъл в SoftUni";
-		break;
-	case2: "Наков ти говори на сън, а ти му задаваш въпроси":
-		$result = "Явно си минал първо ниво";
-		break;
-	case3: "Водиш ожесточен спор с Наков на тема - Алгоритъм за подобрение формулата на алкохола":
-		$result = "Човече, по-добре спирай да сънуваш, че нещата не отиват на добре";
-		break;
-	case4: "Адриана Лима те чака в барчето на университета за да и обясниш разликата между C# и JAVA":
-		$result = "Ставай бързо и отивай преди да са разбрали останалите и да те преварят";
-		break;
-	case5: "Поредната вечер в която успя да свалиш невероятно яки мацки":
-		$result = "Време е да се замислиш за по-голям хард диск";
-		break;
-	case6: "Зала ... тишина ... една камара хора са се втренчили в мониторите си ... от време на време дочуваш тиха псувня":
-		$result = "Братле не спиш!!! На изпит си!!!";
-		break;
-	case7: "Караш супер яка кола по магистралата с висока скорост. Опитваш да спреш, ама педала не работи ....":
-		$result = "Е кво пък толкова, ти все едно не си имал бъгове в твоя код";
-		break;
-	default:
-		$result = "Стига си спал, ами върви си напиши домашното!";
-	break;
-}
-</pre>
+    <header>
+        <h2>SoftUni съновник</h2>
+    </header>
+    
+    <article>
+        <p><span class="operator">switch</span> (<span class="var">$dream</span>) {</p>
+            <p class="pad-1"><span class="operator">case</span> <span class="var"><span class="val">"Наков ти говори на сън"</span>:</p>
+                <p class="pad-2"><span class="var">$result</span> = <span class="val">"Добре дошъл в SoftUni"</span>;</p>
+                <p class="pad-2"><span class="operator">break</span>;</p>
+            <p class="pad-1"><span class="operator">case</span> <span class="val">"Наков ти говори на сън, а ти му задаваш въпроси"</span>:</p>
+                <p class="pad-2"><span class="var">$result</span> = <span class="val">"Явно си минал първо ниво"</span>;</p>
+                <p class="pad-2"><span class="operator">break</span>;</p>
+            <p class="pad-1"><span class="operator">case</span> <span class="val">"Водиш ожесточен спор с Наков на тема - Алгоритъм за подобрение формулата на алкохола"</span>:</p>
+                <p class="pad-2"><span class="var">$result</span> = <span class="val">"Човече, по-добре спирай да сънуваш, че нещата не отиват на добре"</span>;</p>
+                <p class="pad-2"><span class="operator">break</span>;</p>
+            <p class="pad-1"><span class="operator">case</span> <span class="val">"Адриана Лима те чака в барчето на университета за да и обясниш разликата между C# и JAVA"</span>:</p>
+                <p class="pad-2"><span class="var">$result</span> = <span class="val">"Ставай бързо и отивай преди да са разбрали останалите и да те преварят"</span>;</p>
+                <p class="pad-2"><span class="operator">break</span>;</p>
+            <p class="pad-1"><span class="operator">case</span> <span class="val">"Поредната вечер в която успя да свалиш невероятно яки мацки"</span>:</p>
+                <p class="pad-2"><span class="var">$result</span> = <span class="val">"Време е да се замислиш за по-голям хард диск"</span>;</p>
+                <p class="pad-2"><span class="operator">break</span>;</p>
+            <p class="pad-1"><span class="operator">case</span> <span class="val">"Зала ... тишина ... една камара хора са се втренчили в мониторите си ... от време на време дочуваш тиха псувня"</span>:</p>
+                <p class="pad-2"><span class="var">$result</span> = <span class="val">"Братле не спиш!!! На изпит си!!!"</span>;</p>
+                <p class="pad-2"><span class="operator">break</span>;</p>
+            <p class="pad-1"><span class="operator">case</span> <span class="val">"Караш супер яка кола по магистралата с висока скорост. Опитваш да спреш, ама педала не работи ...."</span>:</p>
+                <p class="pad-2"><span class="var">$result</span> = <span class="val">"Е кво пък толкова, ти все едно не си имал бъгове в твоя код"</span>;</p>
+                <p class="pad-2"><span class="operator">break</span>;</p>
+            <p class="pad-1"><span class="operator">default</span>:</p>
+                <p class="pad-2"><span class="var">$result</span> = <span class="val">"Стига си спал, ами върви си напиши домашното!"</span>;</p>
+                <p class="pad-2"><span class="operator">break</span>;</p>
+        <p>}</p>
+    </article>
 </section>
