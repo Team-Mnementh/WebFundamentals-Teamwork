6483a84fe8a08447377e37674dbb7c86d8d3bdd0
N-Hristov <nicolahristov93@gmail.com>
2014-07-02 18:35:57 +0300

    Removed excessive fancybox script

--- framework/views/pictures.php
+++ framework/views/pictures.php
@@ -48,20 +48,4 @@
             <img src="../images/gallery/users-programmers.jpg" alt="snakes" /> </a>
 			
 	</article>
-        
-        <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
-        <script type="text/javascript" src="/fancybox/source/jquery.fancybox.pack.js?v=2.1.5"></script>
-        <script type="text/javascript" src="/fancybox/source/helpers/jquery.fancybox-buttons.js?v=1.0.5"></script>
-        <script type="text/javascript">
-        	$(document).ready(function() {
-                    $(".fancybox").fancybox({
-                        'transitionIn' : 'elastic',
-                        'transitionOut' : 'elastic',
-                        'speedIn'  : 600,
-                        'speedOut'  : 200,
-                        'overlayShow' : false
-                    });                   
-        	});            
-        </script>
-        
 </section>
